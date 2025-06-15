b2842db5ae86ce52986d0e45cd2f6ddf6974766e
Jose Sissa <sissamendoza@outlook.com>
2025-06-15 18:08:45 -0500

    Factory

--- patternsDesigns/decorator.php
+++ patternsDesigns/decorator.php
@@ -5,6 +5,21 @@ interface BudgetInterface
     public function cost(): float;
 }
 
+class DecoratorBudget implements BudgetInterface
+{
+    protected BudgetInterface $budget;
+
+    public function __construct(BudgetInterface $budget)
+    {
+        $this->budget = $budget;
+    }
+
+    public function cost(): float
+    {
+        return $this->budget->cost();
+    }
+}
+
 class BasicBudget implements BudgetInterface
 {
     private int $hours;
@@ -19,21 +34,6 @@ class BasicBudget implements BudgetInterface
     public function cost(): float
     {
         return $this->hours * $this->hourlyRate;
-    }
-}
-
-class DecoratorBudget implements BudgetInterface
-{
-    protected BudgetInterface $budget;
-
-    public function __construct(BudgetInterface $budget)
-    {
-        $this->budget = $budget;
-    }
-
-    public function cost(): float
-    {
-        return $this->budget->cost();
     }
 }
 
