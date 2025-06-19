4a55f5e7d3ba18a3702be28ec1948f357cb02b32
Jose Sissa <sissamendoza@outlook.com>
2025-06-19 11:41:29 -0500

    Architecture structure

---
+++ project/data/BaseData.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\data;
+
+use PDO;
+
+abstract class BaseData
+{
+    protected $pdo;
+
+    public function __construct()
+    {
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8';
+        $this->pdo = new PDO($dsn, DB_USER, DB_PASS);
+        // The next line helps us to handle errors
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+}
